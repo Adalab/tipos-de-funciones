3f38117cb826f288b710eb54eb601628a9f668e1
Carlos Mañas <carlos@spacenomads.com>
2019-07-24 08:48:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+// FUNCIONES NO-FLECHA
+// Función con nombre
+function sum1(num1, num2) {
+  return num1 + num2;
+}
+
+// Función anónima
+const sum2 = function(num1, num2) {
+  return num1 + num2;
+}
+
+
+// Funciones flecha :)
+const sum3 = (num1, num2) => {
+  return num1 + num2;
+}
+
+const sum4 = (num1, num2) => num1 + num2;
